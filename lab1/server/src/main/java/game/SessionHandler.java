91b6260c36733ca9e5c9df1346f48a2c5e4163c2
cherneha <staceychernega@gmail.com>
2018-04-29 16:53:13 +0300

    added maven

---
+++ lab1/server/src/main/java/game/SessionHandler.java
@@ -0,0 +1,16 @@
+package game;
+
+import javax.websocket.Session;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SessionHandler {
+    private final Set<Session> sessions = new HashSet();
+    public void addSession(Session session) {
+        sessions.add(session);
+    }
+
+    public void removeSession(Session session) {
+        sessions.remove(session);
+    }
+}
